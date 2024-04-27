ef6e04e937824f5afb82a9aafc45893d76288562
re-ag <763k357@naver.com>
2024-04-27 21:43:39 +0900

    ACC-41 #comment 바쁘다 바빠

--- models/jaeeunKim/para.m
+++ models/jaeeunKim/para.m
@@ -18,6 +18,7 @@ Ki = 0.003;    % I gain
 encoder_scale = 1/12/4 * 2 * pi;    % rad/s scale
 rpm_scale = 1/12/4*60;
 rpm2encoder = 1/60*2*pi;
+pot2rpm = 5000/1023;
 % Low pass filter for Speed
 num = 1;
 den = [0.03 1];             % Low-Pass filter function: 1/(0.05s + 1)
@@ -27,5 +28,9 @@ Gd = c2d(G, Ts); % c2d 에 필요한 Discrete time(Sampling time) == Ts
 
 num_Gd = Gd.Num{:};         % Low-Pass Filter value
 den_Gd = Gd.Den{:};         % Low-Pass Filter denom value
-%sim("SILS.slx")
-%sim("HILS.slx")
+
+
+ACCmode = 0;
+submode = 0;
+dist = 0.5;
+dir = 0;
